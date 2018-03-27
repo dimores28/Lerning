60cdb5bf9b8b30759ce8c1fb7151eba45944b2f7
Dima <dimores28@rambler.ru>
2018-03-27 22:17:25 +0300

    27032018

--- Grapch_Search/main2.cpp
+++ Grapch_Search/main2.cpp
@@ -1,44 +1,50 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
-int main()
+class Array {
+public:
+	Array();
+	void Out();
+private:
+	static const int n = 10;
+	int A[n][n];
+};
+
+Array::Array()
 {
-	int matrix[10][10];
-	int i(0), j(0), count(0);
-	bool direction(true);
+	int Number = 0;
+	const int maxNumber = n * n;
 
+	for (int i = 0; i < n; i++) {
+		int i2 = i;
+		if (i % 2)
+			for (int j = 0; j <= i; j++, i2--) {
+				A[i2][j] = ++Number;
+				A[n - 1 - i2][n - 1 - j] = (maxNumber - Number + 1);
+			}
 
-	while (count <= 100) {
-		if (i <= 0 && i < 10 && j >= 0)
-		{
-			++i;
-			--j;
-			++count;
-			matrix[i][j] = count;
-		}
-		else if (j <= 0 && j < 10 && i >= 0)
-		{
-			--i;
-			++j;
-			++count;
-			matrix[i][j] = count;
-		}
+		else
+			for (int j = 0; j <= i; j++, i2--) {
+				A[j][i2] = ++Number;
+				A[n - 1 - j][n - 1 - i2] = (maxNumber - Number + 1);
+			}
 	}
+}
 
-
-
-	for (int i(0); i < 10; ++i)
-	{
-		for (int j(0); j < 10; ++j)
-		{
-			cout << matrix[i][j] << " ";
-		}
-
+void Array::Out()
+{
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++)
+			cout << setw(5) << A[i][j];
 		cout << endl;
 	}
+}
 
-	cout << endl;
-
+int main()
+{
+	Array A;
+	A.Out();
 	return 0;
 }
